cf6f8bd5d2cfcdbd9acc2626c520afcef4c1f631
Dang Tran Minh Nhat <supernhat256@gmail.com>
2024-12-27 15:23:53 +0700

    them bien call de so sanh so luong node duoc tao

--- minimax_with_abprunning.py
+++ minimax_with_abprunning.py
@@ -3,16 +3,29 @@ from game import Game
  # search hardcore (naive) 
  # luu cac trang thai trung lap lai
  # giam bot cac trang thai trung lap doi xung nhau
-
-class MinimaxSearch():
+"""
+    Đối với minimax kết hợp alpha beta prunning , node cha cần xử lý node con ngay khi vừa đệ quy xong, để xét xem cần tỉa cây hay không
+    Vì thế phần code của create tree sẽ được sửa về mặt logic và thêm 2 tham số alpha beta khi gọi hàm
+    Việc này giúp giảm trạng thái xuống còn 65591 node so với 549946 node như trước kia ( được tính qua biến call mỗi lần create tree được gọi)
+    Nhược điểm khi sử dụng abprunning:
+        - Vì giải thuật giả sử máy và người chơi đều chơi các nước tối ưu (đánh giá bằng hàm eval)
+        - Abprunning dựa vào đó mà cắt các nhánh chứa các state không tối ưu của cả 2
+        - Khi người chơi đánh nước không tối ưu, máy sẽ không tìm thấy state đó ( do đã bị cắt mất).
+        - Điều này có thể khắc phục bằng cách :
+            +   Dùng hàm đánh giá trạng thái ngay lập tức ( đánh giá không dựa hoàn toàn vào kết quả thắng thua như hàm eval mình đã làm)  
+            +   Chỉ cắt nhánh của máy chơi ( cho phép người chơi đánh nhiều state không tối ưu, còn máy vẫn chỉ đánh 1 state tối ưu)
+"""
+class AlphaBetaPrunning():
     def __init__(self):
         self.init_marks = [['none' for _ in range(3)] for _ in range(3)]
         self.root = State(self.init_marks, 0, 0)
         self.curr = self.root
-        self.call=0
-        # self.create_tree(self.root,self.init_marks,(0,0),0)
-       
-    def create_tree(self, root, marks, index=0, height=0):
+        self.alpha = float ('-inf')
+        self.beta = float ('inf')
+        self.call = 0
+        # self.create_tree(self.root,self.init_marks,(0,0),0,self.alpha,self.beta)
+
+    def create_tree(self, root, marks, index=0, height=0,alpha = float('-inf'),beta =float('inf')):
         self.call+=1
         root.marks = marks
         root.height = height
@@ -20,18 +33,40 @@ class MinimaxSearch():
         root.turn = 'X' if height % 2 == 1 else 'O'
         root.value = 0
         root.children = []
+        prunning = False
         if not root.check_win(): 
             for i in range(3):
                 for j in range(3):
                     if root.marks[i][j] == 'none':
+
                         # can shallow copy vi phep gan bthg o python la deep copy
                         temp_marks = [row.copy() for row in root.marks]
                         temp_marks[i][j] = 'X' if height % 2 == 0 else 'O'
                         child = State(marks=temp_marks, height=height + 1, index=(i,j))
-                        self.create_tree(child, temp_marks, (i,j), height + 1)
+                        temp_alpha = alpha  # shallow cpy vi no la dang float
+                        temp_beta = beta
+                        self.create_tree(child, temp_marks, (i,j), height + 1,temp_alpha,temp_beta)
+                        if(root.height%2 == 0):     # MAX node , update alpha
+                            if child.value > alpha and child.value <beta:
+                                alpha = child.value
+                            elif child.value > beta :
+                                prunning = True
+                    
+                        else:               # MIN node, update beta            
+                            if child.value > alpha and child.value < beta:
+                                beta = child.value
+                            elif child.value < alpha :
+                                prunning = True
                         root.children.append(child)
 
+
+        
+                    if prunning:
+                        break
+                if prunning:
+                    break
         root.eval()
+
     def play(self,game) :
         if game.marks != self.curr.marks:
             for child in self.curr.children :
